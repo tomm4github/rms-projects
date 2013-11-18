f90f3a068399f543b67c21a624032e72c3a50fcb
Tom Morgan <tmorgan@caml15966.local>
2013-11-18 12:06:12 -0800

    Added a resource for random number generation

--- rest-template/src/main/java/com/rms/rest/RestTemplate.java
+++ rest-template/src/main/java/com/rms/rest/RestTemplate.java
@@ -2,7 +2,8 @@ package com.rms.rest;
 
 import com.rms.rest.health.MemoryHealthCheck;
 import com.rms.rest.health.TemplateHealthCheck;
-import com.rms.rest.resource.RestTemplateResource;
+import com.rms.rest.resource.HelloWorldResource;
+import com.rms.rest.resource.RandomNumberResource;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
@@ -23,7 +24,8 @@ public class RestTemplate extends Service<RestTemplateConfiguration> {
     	final String template = configuration.getTemplate();
     	final double memoryCeiling = Double.parseDouble(configuration.getMemoryCeiling());
         final String defaultName = configuration.getDefaultName();
-        environment.addResource(new RestTemplateResource(template, defaultName));
+        environment.addResource(new HelloWorldResource(template, defaultName));
+        environment.addResource(new RandomNumberResource(template, defaultName));
         environment.addHealthCheck(new MemoryHealthCheck(memoryCeiling));
     }
 
